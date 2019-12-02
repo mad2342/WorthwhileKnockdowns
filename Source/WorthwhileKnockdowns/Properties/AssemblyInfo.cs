c60fcfe05cbad6f82323aa6573262c94e39bba7f
Mad <github@typ03.de>
2019-12-02 08:35:57 +0100

    upd for BTG 1.8

--- Source/WorthwhileKnockdowns/Properties/AssemblyInfo.cs
+++ Source/WorthwhileKnockdowns/Properties/AssemblyInfo.cs
@@ -6,5 +6,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("WorthwhileKnockdowns")]
 [assembly: ComVisible(false)]
 [assembly: Guid("4CC1D6FB-85F0-4AFA-BC2E-D77786EE2F8F")]
-[assembly: AssemblyVersion("1.5.0.*")]
+[assembly: AssemblyVersion("1.8.0.*")]
 
