493c0ec500d4fd542fc8f9651622f4277048d1d7
Mad <github@typ03.de>
2020-03-18 14:44:14 +0100

    refactor

---
+++ Source/WorthwhileKnockdowns/Patches/GameLogic.cs
@@ -0,0 +1,60 @@
+﻿using Harmony;
+using BattleTech;
+
+namespace WorthwhileKnockdowns.Patches
+{
+    class GameLogic
+    {
+        // Sprint
+        [HarmonyPatch(typeof(Mech), nameof(Mech.CanSprint), MethodType.Getter)]
+        public static class Mech_CanSprint_Patch
+        {
+            public static bool Prepare()
+            {
+                return WorthwhileKnockdowns.Settings.KnockdownPreventsSprinting;
+            }
+
+            public static void Postfix(Mech __instance, ref bool __result)
+            {
+                bool MechStoodUpThisRound = __instance.StoodUpThisRound;
+
+                if (__result == true)
+                {
+                    // Additional check
+                    if (MechStoodUpThisRound)
+                    {
+                        Logger.Info("[Mech_CanSprint_POSTFIX] Mech COULD sprint BUT just stood up, thus returning false.");
+                        __result = false;
+                    }
+                }
+            }
+        }
+
+
+
+        // Jump
+        [HarmonyPatch(typeof(AbstractActor), nameof(AbstractActor.WorkingJumpjets), MethodType.Getter)]
+        public static class AbstractActor_WorkingJumpjets_Patch
+        {
+            public static bool Prepare()
+            {
+                return WorthwhileKnockdowns.Settings.KnockdownPreventsJumping;
+            }
+
+            public static void Postfix(AbstractActor __instance, ref int __result)
+            {
+                bool ActorStoodUpThisRound = __instance.StoodUpThisRound;
+
+                if (__result > 0)
+                {
+                    // Additional check
+                    if (ActorStoodUpThisRound)
+                    {
+                        Logger.Info("[AbstractActor_WorkingJumpjets_POSTFIX] Mech HAS working jumpjets installed BUT just stood up, reporting zero working jumpjets to temporarily disable his jumping capability.");
+                        __result = 0;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Source/WorthwhileKnockdowns/Patches/UserInterface.cs
@@ -0,0 +1,105 @@
+﻿using Harmony;
+using BattleTech;
+using BattleTech.UI;
+using Localize;
+using System;
+
+namespace WorthwhileKnockdowns.Patches
+{
+    class UserInterface
+    {
+        internal static string SprintDescription = "Use this 'Mech's entire turn to move farther, gaining EVASIVE charges based on distance(+2 Difficulty to hit this unit with ranged attacks per EVASIVE charge). Cannot Sprint if LEGGED, UNSTEADY, or just recovered from a KNOCKDOWN.";
+        internal static string JumpDescription = "Execute a jump move, gaining EVASIVE charges based on distance (+2 Difficulty to hit this unit with ranged attacks per EVASIVE charge). You may also use Jump to initiate a 'Death from Above' attack on nearby enemies. Cannot Jump with all jumpjets destroyed or if just recovered from a KNOCKDOWN.";
+
+        [HarmonyPatch(typeof(CombatHUDSidePanelHoverElement), "InitForSelectionState")]
+        public static class CombatHUDSidePanelHoverElement_InitForSelectionState_Patch
+        {
+            public static void Postfix(CombatHUDSidePanelHoverElement __instance, SelectionType SelectionType, AbstractActor actor)
+            {
+                try
+                {
+                    // THROWS EXCEPTION if there any empty ability slots(SelectionType.None) in MWTray!
+                    // Thus returning early here for all non-relevant SelectionTypes
+                    if (!(SelectionType == SelectionType.Sprint || SelectionType == SelectionType.Jump))
+                    {
+                        return;
+                    }
+                    // All of the following only concerns Sprint or Jump
+
+                    Logger.Info("[CombatHUDSidePanelHoverElement_InitForSelectionState_POSTFIX] SelectionType: " + SelectionType);
+                    Logger.Info("[CombatHUDSidePanelHoverElement_InitForSelectionState_POSTFIX] actor.IsProne: " + actor.IsProne);
+
+                    // Always change description according to settings
+                    if (SelectionType == SelectionType.Sprint && WorthwhileKnockdowns.Settings.KnockdownPreventsSprinting)
+                    {
+                        __instance.Description = new Text(SprintDescription, new object[0]);
+                    }
+                    if (SelectionType == SelectionType.Jump && WorthwhileKnockdowns.Settings.KnockdownPreventsJumping)
+                    {
+                        __instance.Description = new Text(JumpDescription, new object[0]);
+                    }
+
+                    // Always clear warnings
+                    __instance.WarningText = new Text();
+
+                    // Return early if actor is currently prone/shutdown as warning doesn't make sense if unit is still down. At that moment buttons are still disabled anyway...
+                    if (actor.IsProne || actor.IsShutDown)
+                    {
+                        return;
+                    }
+
+
+                    if (SelectionType == SelectionType.Sprint && (actor is Mech mech))
+                    {
+                        Logger.Info("[CombatHUDSidePanelHoverElement_InitForSelectionState_POSTFIX] mech.IsLegged: " + mech.IsLegged);
+                        Logger.Info("[CombatHUDSidePanelHoverElement_InitForSelectionState_POSTFIX] mech.IsUnsteady: " + mech.IsUnsteady);
+                        Logger.Info("[CombatHUDSidePanelHoverElement_InitForSelectionState_POSTFIX] actor.StoodUpThisRound: " + actor.StoodUpThisRound);
+
+                        if (!actor.CanSprint)
+                        {
+                            // Rearranged conditions, see original method...
+                            if (mech.IsLegged)
+                            {
+                                __instance.WarningText = new Text("UNAVAILABLE - DESTROYED LEG PREVENTS SPRINTING", new object[0]);
+                            }
+                            // MAD: Added
+                            else if (actor.StoodUpThisRound && WorthwhileKnockdowns.Settings.KnockdownPreventsSprinting)
+                            {
+                                __instance.WarningText = new Text("UNAVAILABLE - RECENTLY KNOCKED DOWN UNITS CANNOT SPRINT", new object[0]);
+                            }
+                            // :MAD
+                            else if (mech.IsUnsteady)
+                            {
+                                __instance.WarningText = new Text("UNAVAILABLE - UNSTEADY UNITS CANNOT SPRINT", new object[0]);
+                            }
+                        }
+                    }
+                    else if (SelectionType == SelectionType.Jump)
+                    {
+                        bool actorHasJumpjetsInstalled = actor.jumpjets.Count > 0;
+                        Logger.Info("[CombatHUDSidePanelHoverElement_InitForSelectionState_POSTFIX] actorHasJumpjetsInstalled: " + actorHasJumpjetsInstalled);
+                        Logger.Info("[CombatHUDSidePanelHoverElement_InitForSelectionState_POSTFIX] actor.StoodUpThisRound: " + actor.StoodUpThisRound);
+
+                        // Only add warning text if it makes any sense. For mechs that don't even have JJs installed a warning is not sensible...
+                        if (actorHasJumpjetsInstalled)
+                        {
+                            // Note that condition order may not be switched as actor.WorkingJumpjets == 0 is also reported if actor.StoodUpThisRound (See GameLogic.cs)
+                            if (actor.StoodUpThisRound && WorthwhileKnockdowns.Settings.KnockdownPreventsJumping)
+                            {
+                                __instance.WarningText = new Text("UNAVAILABLE - RECENTLY KNOCKED DOWN UNITS CANNOT JUMP", new object[0]);
+                            }
+                            else if (actor.WorkingJumpjets == 0)
+                            {
+                                __instance.WarningText = new Text("UNAVAILABLE - NO OPERATIONAL JUMPJETS LEFT", new object[0]);
+                            }
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Logger.Error(e);
+                }
+            }
+        }
+    }
+}
